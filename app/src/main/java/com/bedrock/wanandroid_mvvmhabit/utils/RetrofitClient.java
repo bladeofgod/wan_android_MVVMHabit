8c5e84b80173f78b336698b422499dfce7fa557c
bladeofgod <450103187@qq.com>
2020-02-15 14:06:03 +0800

    add retrofit client

--- app/src/main/java/com/bedrock/wanandroid_mvvmhabit/utils/RetrofitClient.java
+++ app/src/main/java/com/bedrock/wanandroid_mvvmhabit/utils/RetrofitClient.java
@@ -5,12 +5,28 @@ import android.text.TextUtils;
 
 import java.io.File;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+import me.goldze.mvvmhabit.BuildConfig;
+import me.goldze.mvvmhabit.http.cookie.CookieJarImpl;
+import me.goldze.mvvmhabit.http.cookie.store.PersistentCookieStore;
+import me.goldze.mvvmhabit.http.interceptor.BaseInterceptor;
+import me.goldze.mvvmhabit.http.interceptor.CacheInterceptor;
+import me.goldze.mvvmhabit.http.interceptor.logging.Level;
+import me.goldze.mvvmhabit.http.interceptor.logging.LoggingInterceptor;
 import me.goldze.mvvmhabit.utils.KLog;
 import me.goldze.mvvmhabit.utils.Utils;
 import okhttp3.Cache;
+import okhttp3.ConnectionPool;
 import okhttp3.OkHttpClient;
+import okhttp3.internal.platform.Platform;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * @author LiJiaqi
@@ -63,10 +79,71 @@ public class RetrofitClient {
             KLog.e("Could not create http cache", e);
         }
 
-        Httpu
+        HttpsUtils.SSLParams sslParams = HttpsUtils.getSslSocketFactory();
+        okHttpClient = new OkHttpClient.Builder()
+                .cookieJar(new CookieJarImpl(new PersistentCookieStore(mContext)))
+                //.cache(cache)
+                .addInterceptor(new BaseInterceptor(headers))
+                .addInterceptor(new CacheInterceptor(mContext))
+                .sslSocketFactory(sslParams.sSLSocketFactory,
+                        sslParams.trustManager)
+                .addInterceptor(new LoggingInterceptor
+                        .Builder()
+                        .loggable(BuildConfig.DEBUG)//是否开启日志打印
+                        .setLevel(Level.BASIC)//打印的等级
+                        .log(Platform.INFO)//打印类型
+                        .request("Request")//request 的TAG
+                        .response("Response")// Response的Tag
+                        .addHeader("log-header", "I am the log request header.") // 添加打印头, 注意 key 和 value 都不能是中文
+                        .build()
+                )
+                .connectTimeout(DEFAULT_TIMEOUT, TimeUnit.SECONDS)
+                .writeTimeout(DEFAULT_TIMEOUT, TimeUnit.SECONDS)
+                .connectionPool(new ConnectionPool(
+                        8,15,TimeUnit.SECONDS))
+                .build();
+
+        retrofit = new Retrofit.Builder()
+                .client(okHttpClient)
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .baseUrl(url)
+                .build();
 
 
 
+    }
+
+    /**
+     * create you ApiService
+     * Create an implementation of the API endpoints defined by the {@code service} interface.
+     */
+    public <T> T create(final Class<T> service) {
+        if (service == null) {
+            throw new RuntimeException("Api service is null!");
+        }
+        return retrofit.create(service);
+    }
+
+    /**
+     * /**
+     * execute your customer API
+     * For example:
+     * MyApiService service =
+     * RetrofitClient.getInstance(MainActivity.this).create(MyApiService.class);
+     * <p>
+     * RetrofitClient.getInstance(MainActivity.this)
+     * .execute(service.lgon("name", "password"), subscriber)
+     * * @param subscriber
+     */
+
+    public static <T> T execute(Observable<T> observable, Observer<T> subscriber) {
+        observable.subscribeOn(Schedulers.io())
+                .unsubscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(subscriber);
+
+        return null;
     }
 
 
